c0f62c02c7df7b48861883adebdf9cd2693289df
davidlopez <dfl11@alu.ua.es>
2023-02-04 22:37:25 +0100

    Ejercicio preferencias

--- PersistenciaBasica/ViewController.swift
+++ PersistenciaBasica/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var fechaEdicion: Date?
+    let prefs = UserDefaults()
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var labelView: UILabel!
+    @IBAction func buttonView(_ sender: UIButton) {
+        self.fechaEdicion = Date()
+        prefs.set(self.fechaEdicion, forKey:"fecha")
+        prefs.set(self.textView.text, forKey:"texto")
+        prefs.synchronize()
+        self.labelView.text = DateFormatter.localizedString(
+                     from:self.fechaEdicion!,
+                     dateStyle: .short, timeStyle: .medium)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        prefs.register(defaults:["fecha" : Date(), "texto" : "No hay texto guardado"])
+        self.textView.text = prefs.string(forKey:"texto")
+        let fechaGuardada = prefs.object(forKey:"fecha") as! Date
+        self.labelView.text = DateFormatter.localizedString(
+                     from:fechaGuardada,
+                     dateStyle: .short, timeStyle: .medium)
     }
 
 
